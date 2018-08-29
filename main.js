2d707758df89374b20252d12a83c833e97bc6a06
burntcustard <burntcustard@gmail.com>
2018-08-29 12:12:43 +0100

    JSLinted main.js

--- main.js
+++ main.js
@@ -10,88 +10,97 @@ var headerShown = true; // I.e. whether the full screen banner is up
  */
 function changeTab(navName) {
 
-  var navs = document.getElementById("mainNav");
-  var navAnchors = navs.getElementsByTagName("a");
-  var categories = document.getElementsByClassName("category");
+    "use strict";
 
-  // Buttons:
-  [].forEach.call(navAnchors, function (nav) {
-    if (navName === nav.getAttribute("href")) {
-      nav.classList.add("selected");
-    } else {
-      nav.classList.remove("selected");
-    }
-  });
+    var navs = document.getElementById("mainNav"),
+        navAnchors = navs.getElementsByTagName("a"),
+        categories = document.getElementsByClassName("category");
 
-  // Content:
-  [].forEach.call(categories, function (cat) {
-    if ((navName + "-tab") === cat.id) {
-      cat.classList.add("visible");
-    } else {
-      cat.classList.remove("visible");
-    }
-  });
+    // Buttons:
+    [].forEach.call(navAnchors, function (nav) {
+        if (navName === nav.getAttribute("href")) {
+            nav.classList.add("selected");
+        } else {
+            nav.classList.remove("selected");
+        }
+    });
+
+    // Content:
+    [].forEach.call(categories, function (cat) {
+        if ((navName + "-tab") === cat.id) {
+            cat.classList.add("visible");
+        } else {
+            cat.classList.remove("visible");
+        }
+    });
 
 }
 
 
 function toggleHeader() {
 
-  var header = document.getElementsByTagName("header")[0];
-  var title = header.getElementsByTagName("h1")[0];
-  var websiteDesc = document.getElementById("websiteDesc");
+    "use strict";
 
-  if (header.classList.contains("collapsed")) {
-    changeTab(); // Removes the selections on the tabs
-    header.classList.remove("collapsed");
-    title.innerHTML = title.innerHTML.replace('<a href="#">', '');
-    title.innerHTML = title.innerHTML.replace("</a>", '');
-    headerShown = true;
-  } else {
-    title.innerHTML = '<a href="#">' + title.innerHTML + '</a>';
-    header.classList.add("collapsed");
-    headerShown = false;
-  }
+    var header = document.getElementsByTagName("header")[0],
+        title = header.getElementsByTagName("h1")[0],
+        websiteDesc = document.getElementById("websiteDesc");
+
+    if (header.classList.contains("collapsed")) {
+        changeTab(); // Removes the selections on the tabs
+        header.classList.remove("collapsed");
+        title.innerHTML = title.innerHTML.replace('<a href="#">', '');
+        title.innerHTML = title.innerHTML.replace("</a>", '');
+        headerShown = true;
+    } else {
+        title.innerHTML = '<a href="#">' + title.innerHTML + '</a>';
+        header.classList.add("collapsed");
+        headerShown = false;
+    }
 
 }
 
 
 // Detect hash changes in quite a heavy handed way,
 // then toggle header and/or change highlighted navigation
-window.addEventListener('popstate', function(event) {
-  var hash = window.location.hash;
-  if (hash === '' || hash === '#') {
-    toggleHeader();
-  } else {
-    if (headerShown) {
-      toggleHeader();
+window.addEventListener('popstate', function (event) {
+
+    "use strict";
+
+    var hash = window.location.hash;
+    if (hash === '' || hash === '#') {
+        toggleHeader();
+    } else {
+        if (headerShown) {
+            toggleHeader();
+        }
+        changeTab(hash);
     }
-    changeTab(hash);
-  }
 });
 
 
 window.onload = function () {
 
-  var title = document.getElementsByTagName("h1")[0];
+    "use strict";
 
-  // If page is loaded with a hash, load the correct tab:
-  if (window.location.hash) {
-    toggleHeader();
-    changeTab(window.location.hash);
-  }
+    var title = document.getElementsByTagName("h1")[0];
 
-  title.onclick = function() {
-    if (!headerShown) {
-      window.location.hash = '';
+    // If page is loaded with a hash, load the correct tab:
+    if (window.location.hash) {
+        toggleHeader();
+        changeTab(window.location.hash);
     }
-  };
 
-  // Scrolling down on homepage goes to first tab
-  window.addEventListener("mousewheel", function (e) {
-    if (e.wheelDeltaY < 0 && window.location.hash === '') {
-      window.location.hash = "#about";
-    }
-  }, false);
+    title.onclick = function () {
+        if (!headerShown) {
+            window.location.hash = '';
+        }
+    };
+
+    // Scrolling down on homepage goes to first tab
+    window.addEventListener("mousewheel", function (e) {
+        if (e.wheelDeltaY < 0 && window.location.hash === '') {
+            window.location.hash = "#about";
+        }
+    }, false);
 
 };
