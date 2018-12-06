11c643895f1eaf9283a664cb1c56247320ef1bab
burntcustard <burntcustard@gmail.com>
2018-12-06 15:42:04 +0000

    Start of big restyle

--- main.js
+++ main.js
@@ -75,104 +75,104 @@ window.onload = function () {
 
     'use strict';
 
-    // If page is loaded with a hash, load the correct tab:
-    if (window.location.hash) {
-        toggleHeader();
-        //changeTab(window.location.hash);
-    }
-
-    // Toggle the header if the title in the small nav bar is clicked
-    document.getElementsByTagName('h1')[0].onclick = function () {
-        if (!headerShown) {
-            window.location.hash = '';
-        }
-        return false;
-    };
-
-    // Scrolling down on homepage goes to first tab
-    window.addEventListener('wheel', event => {
-        if (event.deltaY > 0 && window.location.hash === '') {
-            window.location.hash = document.querySelector('main > section').id;
-        }
-    }, false);
-
-    // Async image (or anything) loading by swapping data-src string into src
-    document.querySelectorAll('[data-src]').forEach(element => {
-        element.setAttribute('src', element.getAttribute('data-src'));
-    });
-
-    // Set IDs of articles and related ARIA-labelledbys
-    document.querySelectorAll('section').forEach((category) => {
-        category.querySelectorAll('article').forEach((article) => {
-            var heading = article.querySelector('h2');
-            if (heading && heading.innerHTML) {
-                var id = strToId(category.id + '-' + heading.innerHTML);
-                heading.id = id;
-                article.setAttribute('aria-labelledby', id);
-            }
-        });
-    });
-
-    // Add intersectionObservers for the categories
-
-    // Links to sections on same page
-    //const secHeader = document.querySelector('.c-header--secondary');
-    var nav = document.querySelector('header > nav');
-
-    var externalNavLinks = nav.querySelectorAll('a:not([href*="#"])');
-
-    externalNavLinks.forEach(link => {
-        link.classList.remove('clicked');
-        link.onclick = function() {
-            this.classList.add('clicked');
-        }
-    });
-
-    //const internalNavLinks = secHeader.querySelectorAll('a[href*="#"]');
-    var internalNavLinks = nav.querySelectorAll('a[href*="#"]');
-
-    // Get the sections that match the links in the nav
-    var sections = [...internalNavLinks].map(
-        link => document.getElementById(link.href.split("#").pop())
-    );
-
-    console.log(sections);
-
-    var observerConfig = {
-        threshold: Array.from(Array(100), (x, i) => i * .01)
-    };
-
-    var currentLinkObserver = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            let onscreenRatio = (entry.intersectionRatio * entry.target.scrollHeight) / window.innerHeight;
-            if (onscreenRatio >= 0.5) {
-                internalNavLinks.forEach(navLink => {
-                    if (navLink.classList.contains('observed')) {
-                        navLink.classList.remove('observed');
-                        navLink.classList.remove('clicked');
-                    }
-                });
-                let currentNavLink = nav.querySelector(`a[href="#${entry.target.id}"]`);
-                currentNavLink.classList.add('observed');
-            }
-            // if (onscreenRatio < 0.5) {
-            //     let currentNavLink = nav.querySelector('.observed');
-            //     if (currentNavLink) {
-            //         let currentNavLinkHref = currentNavLink.href.split("#").pop();
-            //         let entryHref = entry.target.id;
-            //         if (currentNavLinkHref === entryHref) {
-            //             currentNavLink.classList.remove('observed');
-            //         }
-            //     }
-            // }
-        });
-
-    }, observerConfig);
-
-    sections.forEach(section => {
-        if (section) {
-            currentLinkObserver.observe(section);
-        }
-    });
+    // // If page is loaded with a hash, load the correct tab:
+    // if (window.location.hash) {
+    //     toggleHeader();
+    //     //changeTab(window.location.hash);
+    // }
+    //
+    // // Toggle the header if the title in the small nav bar is clicked
+    // document.getElementsByTagName('h1')[0].onclick = function () {
+    //     if (!headerShown) {
+    //         window.location.hash = '';
+    //     }
+    //     return false;
+    // };
+    //
+    // // Scrolling down on homepage goes to first tab
+    // window.addEventListener('wheel', event => {
+    //     if (event.deltaY > 0 && window.location.hash === '') {
+    //         window.location.hash = document.querySelector('main > section').id;
+    //     }
+    // }, false);
+    //
+    // // Async image (or anything) loading by swapping data-src string into src
+    // document.querySelectorAll('[data-src]').forEach(element => {
+    //     element.setAttribute('src', element.getAttribute('data-src'));
+    // });
+    //
+    // // Set IDs of articles and related ARIA-labelledbys
+    // document.querySelectorAll('section').forEach((category) => {
+    //     category.querySelectorAll('article').forEach((article) => {
+    //         var heading = article.querySelector('h2');
+    //         if (heading && heading.innerHTML) {
+    //             var id = strToId(category.id + '-' + heading.innerHTML);
+    //             heading.id = id;
+    //             article.setAttribute('aria-labelledby', id);
+    //         }
+    //     });
+    // });
+    //
+    // // Add intersectionObservers for the categories
+    //
+    // // Links to sections on same page
+    // //const secHeader = document.querySelector('.c-header--secondary');
+    // var nav = document.querySelector('header > nav');
+    //
+    // var externalNavLinks = nav.querySelectorAll('a:not([href*="#"])');
+    //
+    // externalNavLinks.forEach(link => {
+    //     link.classList.remove('clicked');
+    //     link.onclick = function() {
+    //         this.classList.add('clicked');
+    //     }
+    // });
+    //
+    // //const internalNavLinks = secHeader.querySelectorAll('a[href*="#"]');
+    // var internalNavLinks = nav.querySelectorAll('a[href*="#"]');
+    //
+    // // Get the sections that match the links in the nav
+    // var sections = [...internalNavLinks].map(
+    //     link => document.getElementById(link.href.split("#").pop())
+    // );
+    //
+    // console.log(sections);
+    //
+    // var observerConfig = {
+    //     threshold: Array.from(Array(100), (x, i) => i * .01)
+    // };
+    //
+    // var currentLinkObserver = new IntersectionObserver((entries) => {
+    //     entries.forEach(entry => {
+    //         let onscreenRatio = (entry.intersectionRatio * entry.target.scrollHeight) / window.innerHeight;
+    //         if (onscreenRatio >= 0.5) {
+    //             internalNavLinks.forEach(navLink => {
+    //                 if (navLink.classList.contains('observed')) {
+    //                     navLink.classList.remove('observed');
+    //                     navLink.classList.remove('clicked');
+    //                 }
+    //             });
+    //             let currentNavLink = nav.querySelector(`a[href="#${entry.target.id}"]`);
+    //             currentNavLink.classList.add('observed');
+    //         }
+    //         // if (onscreenRatio < 0.5) {
+    //         //     let currentNavLink = nav.querySelector('.observed');
+    //         //     if (currentNavLink) {
+    //         //         let currentNavLinkHref = currentNavLink.href.split("#").pop();
+    //         //         let entryHref = entry.target.id;
+    //         //         if (currentNavLinkHref === entryHref) {
+    //         //             currentNavLink.classList.remove('observed');
+    //         //         }
+    //         //     }
+    //         // }
+    //     });
+    //
+    // }, observerConfig);
+    //
+    // sections.forEach(section => {
+    //     if (section) {
+    //         currentLinkObserver.observe(section);
+    //     }
+    // });
 
 };
