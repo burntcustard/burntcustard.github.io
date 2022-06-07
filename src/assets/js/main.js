cb8718aa01c7de04e590edef38c30f8bd85955d9
burntcustard <burntcustard@gmail.com>
2022-06-07 03:00:36 +0100

    Improve fancy mouse button effect JS

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,19 @@
 const fancyBorderElements = document.querySelectorAll('.listing a, nav a');
 
-fancyBorderElements.forEach((element) => {
-  window.addEventListener('mousemove', ({ clientX, clientY }) => {
-    const { x, y, width, height } = element.getBoundingClientRect();
-    const dx = clientX - (x + .5 * width);
-    const dy = clientY - (y + .5 * height);
-    const angle = Math.atan2(dy, dx) * 180 / Math.PI;
-    element.style.setProperty('--deg', `${angle + 90}deg`);
-})});
+function setAngleToMouse(element, mx, my) {
+  const { x, y, width, height } = element.getBoundingClientRect();
+  const dx = mx - (x + width / 2);
+  const dy = my - (y + height / 2);
+  const angle = Math.atan2(dy, dx) * 180 / Math.PI;
+  element.style.setProperty('--deg', `${angle + 90}deg`);
+}
+
+window.addEventListener('mousemove', ({ clientX, clientY }) => {
+  sessionStorage.setItem('mx', clientX);
+  sessionStorage.setItem('my', clientY);
+  fancyBorderElements.forEach(element => setAngleToMouse(element, clientX, clientY));
+});
+
+const mx = sessionStorage.getItem('mx');
+const my = sessionStorage.getItem('my');
+fancyBorderElements.forEach(element => mx && my && setAngleToMouse(element, mx, my));
