2d9e1452d370ba4f1df7bc035137c65ef38f6ac3
burntcustard <burntcustard@gmail.com>
2022-06-26 14:21:35 +0100

    Potential fix to stop fancy borders being done on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,26 @@
-const fancyBorderElements = [...document.querySelectorAll('.listing > a, nav a')];
+function applyFancyBorders() {
+  const fancyBorderElements = [...document.querySelectorAll('.listing > a, nav a')];
 
-function setAngleToMouse(element, mx, my) {
-  const { x, y, width, height } = element.getBoundingClientRect();
-  const dx = mx - (x + width / 2);
-  const dy = my - (y + height / 2);
-  const angle = Math.atan2(dy, dx) * 180 / Math.PI;
-  element.style.setProperty('--deg', `${angle + 90}deg`);
+  function setAngleToMouse(element, mx, my) {
+    const { x, y, width, height } = element.getBoundingClientRect();
+    const dx = mx - (x + width / 2);
+    const dy = my - (y + height / 2);
+    const angle = Math.atan2(dy, dx) * 180 / Math.PI;
+    element.style.setProperty('--deg', `${angle + 90}deg`);
+  }
+
+  window.addEventListener('mousemove', ({ clientX, clientY }) => {
+    sessionStorage.setItem('mx', clientX);
+    sessionStorage.setItem('my', clientY);
+    fancyBorderElements.forEach(element => setAngleToMouse(element, clientX, clientY));
+  });
+
+  const mx = sessionStorage.getItem('mx');
+  const my = sessionStorage.getItem('my');
+  mx && my && fancyBorderElements.forEach(element => setAngleToMouse(element, mx, my));
 }
 
-window.addEventListener('mousemove', ({ clientX, clientY }) => {
-  sessionStorage.setItem('mx', clientX);
-  sessionStorage.setItem('my', clientY);
-  fancyBorderElements.forEach(element => setAngleToMouse(element, clientX, clientY));
-});
-
-const mx = sessionStorage.getItem('mx');
-const my = sessionStorage.getItem('my');
-mx && my && fancyBorderElements.forEach(element => setAngleToMouse(element, mx, my));
+if (window.matchMedia('(pointer: fine)')) {
+  console.log('pointer: fine');
+  applyFancyBorders();
+}
