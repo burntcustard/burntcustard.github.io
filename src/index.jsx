27071f555133b8fcbd5f740ab7f65872ca8f824e
burntcustard <burntcustard@gmail.com>
2022-06-26 18:16:51 +0100

    fadein homepage stuff

--- src/index.jsx
+++ src/index.jsx
@@ -10,37 +10,63 @@ function Index() {
 
     if (!localStorage.getItem('first')) {
       localStorage.setItem('first', true);
-      elements.push(document.querySelector('nav'));
+      const nav = document.querySelector('nav');
+      nav.dataset.fadeinDelay = 500;
+      elements.push(nav);
     }
 
-    elements.forEach((item, i) => {
-      item.style.opacity = 0;
-      item.style.transition = 'opacity 500ms';
+    let runningTotalDelay = 0;
+
+    elements.forEach((element, i) => {
+      runningTotalDelay += parseInt(element.dataset.fadeinDelay);
+      element.style.opacity = 0;
+      element.style.transition = 'opacity 500ms';
+
       setTimeout(() => {
-        item.style.opacity = 1;
-        setTimeout(() => item.style.transition = '', 400);
-      }, i * 400 + 200);
+        element.style.opacity = 1;
+        setTimeout(() => element.style.transition = '', 400);
+      }, runningTotalDelay);
     });
   `;
 
   return (
     <HTMLPage>
       <header>
-        <h1>{pkg.name}</h1>
+        <h1 data-fadein-delay="300">{pkg.name}</h1>
       </header>
 
-      <p>
+      <p data-fadein-delay="500">
         Hi! My name's John, aka burntcustard. I’m a senior front-end web developer at <a href="https://wearelighthouse.com/">Lighthouse London</a>, and a Computer Science (Games) graduate.
       </p>
 
       <p>
-        <a href="https://twitter.com/burntcustard">Twitter</a>
+        <a
+          href="https://twitter.com/burntcustard"
+          data-fadein-delay="500"
+        >
+          Twitter
+        </a>
         {' '}
-        <a href="mailto:burntcustard@gmail.com">Email</a>
+        <a
+          href="mailto:burntcustard@gmail.com"
+          data-fadein-delay="300"
+        >
+          Email
+        </a>
         {' '}
-        <a href="https://github.com/burntcustard">GitHub</a>
+        <a
+          href="https://github.com/burntcustard"
+          data-fadein-delay="300"
+        >
+          GitHub
+        </a>
         {' '}
-        <a href="https://codepen.io/burntcustard/">CodePen</a>
+        <a
+          href="https://codepen.io/burntcustard/"
+          data-fadein-delay="300"
+        >
+          CodePen
+        </a>
       </p>
 
       <script children={null} dangerouslySetInnerHTML={{ __html: script }}/>
