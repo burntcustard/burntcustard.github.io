05a448d55fb662b22d4abeefc291f291f344c34f
burntcustard <burntcustard@gmail.com>
2022-06-29 01:06:08 +0100

    Test preloading the Google font stylesheet

--- src/_includes/components/head.jsx
+++ src/_includes/components/head.jsx
@@ -5,10 +5,10 @@ const Head = ({ title, description }) => (
     <meta name="theme-color" content="#ffffff"/>
     {description && <meta name="description" content={description}/>}
     <title>{title}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-    <link href="/assets/css/style.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Sans:wght@600&display=swap" rel="preload" as="style" />
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Sans:wght@600&display=swap" rel="stylesheet"/>
+    <link href="/assets/css/style.css" rel="stylesheet"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
