80b86b6e7f95581a645bc38401fe34cd7c22406a
burntcustard <burntcustard@gmail.com>
2022-07-19 01:20:12 +0100

    Replace font links with what's now suggested on Google Fonts

--- src/_includes/components/head.jsx
+++ src/_includes/components/head.jsx
@@ -5,9 +5,9 @@ const Head = ({ title, description }) => (
     <meta name="theme-color" content="#ffffff"/>
     {description && <meta name="description" content={description}/>}
     <title>{title}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Sans:wght@600&display=swap" rel="preload" as="style" />
-    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Sans:wght@600&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&family=Fira+Sans:wght@700&display=swap" rel="stylesheet"/>
     <link href="/assets/css/style.css" rel="stylesheet"/>
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
