2ae8efe4719ac0ce51d339100cc44479f7c761c3
burntcustard <burntcustard@gmail.com>
2022-08-13 01:52:34 +0100

    Messy WIP CSS tweaks for content, new CodePen embed, WIP article

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const reactSSRPlugin = require('eleventy-plugin-react-ssr');
+const codepenEmbed = require('@kevingimbel/eleventy-plugin-codepen');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(reactSSRPlugin);
+  eleventyConfig.addPlugin(codepenEmbed);
 
   eleventyConfig.setBrowserSyncConfig({
     files: 'dist/assets/css/style.css',
