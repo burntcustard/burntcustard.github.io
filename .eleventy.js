ccb9846410037c35a445f8f02e1abf4591e9a729
burntcustard <burntcustard@gmail.com>
2022-08-18 01:05:45 +0100

    Roll our own CodePen embed wrapper for a little more flexibility

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,47 @@
 const reactSSRPlugin = require('eleventy-plugin-react-ssr');
 const anchorsPlugin = require('@orchidjs/eleventy-plugin-ids');
-const codepenEmbed = require('@kevingimbel/eleventy-plugin-codepen');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(reactSSRPlugin);
   eleventyConfig.addPlugin(anchorsPlugin);
-  eleventyConfig.addPlugin(codepenEmbed);
 
   eleventyConfig.setBrowserSyncConfig({
     files: [
       'dist/assets/css/style.css',
       'dist/assets/js/main.js',
     ],
+  });
+
+  eleventyConfig.addShortcode("codepen_js", () => {
+    return `<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>`;
+  });
+
+  eleventyConfig.addShortcode("codepen", (url, config = '') => {
+    const {
+      height = 256,
+      theme = 'dark',
+      tabs = 'result',
+      title = 'Unknown Pen',
+      preview = false,
+      editable = false,
+    } = Object.fromEntries(config.split(';').map(c => c.split(':')));
+
+    const [,,, user,, slug] = url.split('/');
+
+    return `<p
+      data-height="${height}"
+      data-theme-id="${theme}"
+      data-slug-hash="${slug}"
+      data-default-tab="${tabs}"
+      data-user="${user}"
+      data-pen-title="${title}"
+      data-preview="${preview}"
+      data-editable="${editable}"
+      class="codepen"
+      style="height:${height}px"
+    >
+      See the <a href="https://codepen.io/${user}/pen/${slug}/">demo on CodePen</a>
+    </p>`;
   });
 
   eleventyConfig.addPassthroughCopy('assets/img');
