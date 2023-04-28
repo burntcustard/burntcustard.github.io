07a67ab37d64fadbf22059fd0b9ee3a1b86376ee
burntcustard <burntcustard@gmail.com>
2023-04-28 16:46:07 +0100

    Remove fade-in script because it was a bit unreliable

--- src/index.jsx
+++ src/index.jsx
@@ -5,28 +5,6 @@ import HTMLPage from './_includes/components/html-page';
 function Index() {
   const { collections, data, pkg } = useContext(EleventyContext);
 
-  const script = `
-    if (!localStorage.getItem('first')) {
-      localStorage.setItem('first', true);
-      const nav = document.querySelector('nav');
-      let elements = [...document.querySelectorAll('h1, header + p, header + p + p a')];
-      let runningTotalDelay = 0;
-      nav.dataset.fadeinDelay = 500;
-      elements.push(nav);
-
-      elements.forEach(element => {
-        runningTotalDelay += parseInt(element.dataset.fadeinDelay);
-        element.style.opacity = 0;
-        element.style.transition = 'opacity 500ms';
-
-        setTimeout(() => {
-          element.style.opacity = '';
-          setTimeout(() => element.style.transition = '', 400);
-        }, runningTotalDelay);
-      });
-    }
-  `;
-
   return (
     <HTMLPage>
       <header>
@@ -70,8 +48,6 @@ function Index() {
           CodePen
         </a>
       </p>
-
-      <script dangerouslySetInnerHTML={{ __html: script }}/>
     </HTMLPage>
   )
 }
