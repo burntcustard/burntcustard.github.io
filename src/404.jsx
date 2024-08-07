313a51b63e7b825352ba68c52de8bcd0571272c9
John <burntcustard@gmail.com>
2024-08-07 17:01:31 +0100

    Attempt to add custom 404 page

---
+++ src/404.jsx
@@ -0,0 +1,17 @@
+import HTMLPage from './_includes/components/html-page';
+
+function Index() {
+  return (
+    <HTMLPage>
+      <header>
+        <h1>404</h1>
+      </header>
+
+      <p>
+        Page cannot be found. <a  href="/">Return to burnt.io</a>
+      </p>
+    </HTMLPage>
+  )
+}
+
+export default Index;
