003028ca96084beb8f2c484c760a4771ecb67057
burntcustard <burntcustard@gmail.com>
2024-11-30 19:22:10 +0000

    Add Bluesky, for now at least

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,14 @@ import HTMLPage from './_includes/components/html-page';
 function Index() {
   const { pkg } = useContext(EleventyContext);
 
+  const socials = {
+    'Twitter': 'https://x.com/burntcustard',
+    'Bluesky': 'https://bsky.app/profile/burnt.io',
+    'LinkedIn': 'https://www.linkedin.com/in/burntcustard/',
+    'GitHub': 'https://github.com/burntcustard',
+    'CodePen': 'https://codepen.io/burntcustard/',
+  };
+
   return (
     <HTMLPage>
       <header>
@@ -23,34 +31,18 @@ function Index() {
         <strong>I&apos;m currently looking for new projects</strong> - full time and freelance. You can see <a href="/john-evans-cv">my CV</a>, check out my previous <a href="/work">work</a>, or send an enquiry to <a href="mailto:john@burnt.io?subject=Enquiry">john@burnt.io</a>.
       </p>
 
-      <p>
-        <a
-          href="https://twitter.com/burntcustard"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        {' '}
-        <a
-          href="https://github.com/burntcustard"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        {' '}
-        <a
-          href="https://codepen.io/burntcustard/"
-          rel="noreferrer"
-        >
-          CodePen
-        </a>
-        {' '}
-        <a
-          href="https://www.linkedin.com/in/burntcustard/"
-          rel="noreferrer"
-        >
-          LinkedIn
-        </a>
+      <p style={{ textWrap: 'balance' }}>
+        {Object.entries(socials).map(([name, url], index) => (
+          <>
+            {index ? ' ' : ''}
+            <a
+              href={url}
+              rel="noreferrer"
+            >
+              {name}
+            </a>
+          </>
+        ))}
       </p>
     </HTMLPage>
   )
