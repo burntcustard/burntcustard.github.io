661cbcf7ce0eb6949ec137e9e219e7d3ad2d1568
John Evans <john@wearelighthouse.com>
2026-09-19 19:18:45 +0100

    Remove webmanifest to prevent browsers thinking the site could be installed as a PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import webmanifest from 'astro-webmanifest';
 import { redirects } from './src/data/redirects';
 
 export default defineConfig({
@@ -11,12 +10,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
-    webmanifest({
-      name: 'burnt.io',
-      icon: 'public/favicon.svg',
-      theme_color: "#ffcc11",
-      background_color: "#222222",
-    }),
   ],
   prefetch: true,
 });
